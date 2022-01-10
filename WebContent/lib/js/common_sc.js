08b55daeb33db26a7b6bc2d8617544bf7854f99f
soso2234 <mingi371@naver.com>
2022-01-10 11:09:30 +0900

    장바구니 카운트 추가, alert 메세지 수정

--- WebContent/lib/js/common_sc.js
+++ WebContent/lib/js/common_sc.js
@@ -43,11 +43,11 @@ function addCart(item_seq){
 					return alert("장바구니에 추가되었습니다.");
 					
 				}
-			} else {
-				alert("장바구니 추가 오류입니다.");		
-			}
+		  } else {
+			  return alert("품절된 상품입니다.");		
+		  }
 		});     
    } else {
-	   return;
+	   return alert("품절된 상품입니다.");
    }
 }
